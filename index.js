a69fa757a9ed1ef8ea2dfff39b6b0a19f2923dd5
Margaux <margaux.ems@gmail.com>
2026-02-20 17:06:47 +0100

    ajout script start et cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 
@@ -11,6 +12,7 @@ cloudinary.config({
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const userRouter = require("./routes/user");
 const offerRoutes = require("./routes/offer");
